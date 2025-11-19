3f72b7c25c26b2f2d02015fcd962d3f8dc345381
Hennedys <hennedys.guerra26@compscihigh.org>
2025-11-19 05:10:23 +0000

    Initial commit

--- api.js
+++ api.js
@@ -2,6 +2,25 @@ const env = require('dotenv').config({ quiet: true });
 const prompt = require('prompt-sync')();
 const apiKey = process.env.apiKey2
 
+let url = "https://api.api-onepiece.com/v2/characters/en"
+
+// run your code with node api.js
+// add your apiKey to .env if your API needs one, otherwise ignore
+// Follow the checklist on the Performance Assessment Google Doc
+console.log("Welcome to the one piece page: ")
+let characterC = prompt("What Character Do You Want Info On?(Integers Only)")
+let requestUrl = url + "/" + characterC
+
+async function CharacterGenerator(url){
+
+const response = await fetch(requestUrl);
+const data = await response.json();
+console.log(data);
+}
+
+console.log("Welcome to character Gen")
+CharacterGenerator(requestUrl)
+
 // run your code with node api.js
 // add your apiKey to .env if your API needs one, otherwise ignore
 // Follow the checklist on the Performance Assessment Google Doc
--- chatgpt.js
+++ chatgpt.js
@@ -5,3 +5,25 @@ const apiKey = process.env.apiKey1
 // run your code with node chatgpt.js
 // add your apiKey to .env from gChat
 // Follow the checklist on the Performance Assessment Google Doc
+
+let requestUrl = "https://api.openai.com/v1/responses"
+
+async function skibidi (){
+const options = {
+method:'POST',
+headers:{
+Authorization:'Bearer ' + apiKey,
+'Content-Type': 'application/json',
+},
+body:JSON.stringify({
+    model: prompt("What model do you want to use: "),
+    input: prompt("What would you like to ask the chatbox: ")
+})
+}
+const response = await fetch(requestUrl, options);
+const r = await response.json();
+console.log(r);
+}
+console.log("Welcome to ChatGPT")
+skibidi(requestUrl)
+e
